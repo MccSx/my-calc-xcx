aa860a7af2ebce5cc881669cd83fb5546388d84f
MccSx <shixiangcool@163.com>
2019-03-06 22:03:21 +0800

    完成计算公式放入临时数组

--- pages/calc/calc.js
+++ pages/calc/calc.js
@@ -1,12 +1,39 @@
 Page({
     data: {
         result: '0',
-        activeMes: ''
+        activeMes: '',
+        tempArr: [],
+        tempStr: ''
     },
     inputOrder(e) {
         this.setData({
             activeMes: ''
         })
+        let eleID = e.target.id
+        let tempNum = e.target.id.slice(3)
+        let arr = ['0','1','2','3','4','5','6','7','8','9']
+        if (arr.indexOf(tempNum) >= 0 || eleID === 'spot') {
+            //点击的按钮是0-9或者是小数点
+            if (eleID === 'spot') {
+                this.data.tempStr += '.'
+            } else {
+                this.data.tempStr += tempNum
+            }
+            this.setData({
+                result: this.data.tempStr
+            })
+        } else if (eleID === 'plus') {
+            //点击的按钮是‘+’
+            if(this.data.tempStr) {
+                this.data.tempArr.push(this.data.tempStr)
+                this.data.tempArr.push('+')
+                this.setData({
+                    tempStr: ''
+                })
+            }
+        }
+        // console.log(this.data.tempStr)
+
     },
     startOrder(e) {
         this.setData({
