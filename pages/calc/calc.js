90b1328f718c45084b6271955f66702be162693e
MccSx <shixiangcool@163.com>
2019-03-07 20:38:36 +0800

    完成计算功能

--- pages/calc/calc.js
+++ pages/calc/calc.js
@@ -1,6 +1,7 @@
 Page({
     data: {
         result: '0',
+        tempResult: '',
         activeMes: '',
         tempArr: [],
         tempStr: ''
@@ -16,28 +17,76 @@ Page({
             //点击的按钮是0-9或者是小数点
             if (eleID === 'spot') {
                 this.data.tempStr += '.'
+                this.data.tempResult += '.'
             } else {
                 this.data.tempStr += tempNum
+                this.data.tempResult += tempNum
             }
             this.setData({
-                result: this.data.tempStr
+                result: this.data.tempResult
             })
         } else if (eleID === 'plus') {
             //点击的按钮是‘+’
-            if(this.data.tempStr) {
-                this.data.tempArr.push(this.data.tempStr)
-                this.data.tempArr.push('+')
-                this.setData({
-                    tempStr: ''
-                })
-            }
+            this.recordOperator('+')
+        } else if (eleID === 'reduce') {
+            //点击的按钮是‘-’
+            this.recordOperator('-')
+        } else if (eleID === 'ride') {
+            //点击的按钮是‘×’
+            this.recordOperator('×')
+        } else if (eleID === 'except') {
+            //点击的安妮是‘÷’
+            this.recordOperator('÷')
+        } else if (eleID === 'equal') {
+            this.data.tempArr.push( parseFloat(this.data.tempStr) )
+            let res = this.calculation(this.data.tempArr)
+            this.setData({
+                tempStr: '',
+                tempResult: '',
+                tempArr: [],
+                result: res[0]+'' 
+            })
         }
-        // console.log(this.data.tempStr)
+        // console.log(this.data.tempArr)
 
     },
     startOrder(e) {
         this.setData({
             activeMes: e.target.id
         })
+    },
+    recordOperator(op) {
+        if (this.data.tempStr) {
+            this.data.tempArr.push( parseFloat(this.data.tempStr) )
+            this.data.tempArr.push(op)
+            this.data.tempResult += op
+            this.setData({
+                tempStr: '',
+                result: this.data.tempResult
+            })
+        }
+    },
+    calculation(arr) {
+        while (arr.indexOf('÷') >= 0) {
+            let i = arr.indexOf('÷')
+            let tempRes = arr[i - 1] / arr[i + 1]
+            arr.splice(i - 1, 3, tempRes)
+        }
+        while (arr.indexOf('×') >= 0) {
+            let i = arr.indexOf('×')
+            let tempRes = arr[i - 1] * arr[i + 1]
+            arr.splice(i - 1, 3, tempRes)
+        }
+        while (arr.indexOf('+') >= 0) {
+            let i = arr.indexOf('+')
+            let tempRes = arr[i - 1] + arr[i + 1]
+            arr.splice(i - 1, 3, tempRes)
+        }
+        while (arr.indexOf('-') >= 0) {
+            let i = arr.indexOf('-')
+            let tempRes = arr[i - 1] - arr[i + 1]
+            arr.splice(i - 1, 3, tempRes)
+        }
+        return arr
     }
 })
